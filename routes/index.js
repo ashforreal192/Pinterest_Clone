ce372e474ace8cf446082c5a9a61ad6e6f60e5f9
Shatodru <shatodru2003@gmail.com>
2024-08-11 21:57:23 +0530

    11/08

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,25 @@ router.get('/register', function (req, res, next) {
 });
 
 router.get('/profile', isLoggedIn, async function (req, res, next) {
+  const user =
+    await userModel
+      .findOne({ username: req.session.passport.user })
+      .populate("posts")
+  res.render('show', { user, nav: true });
+});
+
+router.get('/show/posts', isLoggedIn, async function (req, res, next) {
   const user = await userModel.findOne({ username: req.session.passport.user })
+  const posts = await postModel.find().populate("user")
+
+  res.render("feed", { user, posts, nav: true })
+});
+
+router.get('/feed', isLoggedIn, async function (req, res, next) {
+  const user =
+    await userModel
+      .findOne({ username: req.session.passport.user })
+      .populate("posts")
   res.render('profile', { user, nav: true });
 });
 
@@ -30,12 +48,17 @@ router.get('/add', isLoggedIn, async function (req, res, next) {
 
 router.post('/createpost', isLoggedIn, upload.single("postimage"), async function (req, res, next) {
   const user = await userModel.findOne({ username: req.session.passport.user })
-  postModel.create({
+  const post = await postModel.create({
     user: user._id,
     title: req.body.title,
     descrition: req.body.descrition,
     image: req.file.filename
   })
+
+
+  user.posts.push(post._id)
+  await user.save()
+  res.redirect("/profile")
 });
 
 router.post('/fileupload', isLoggedIn, upload.single("image"), async function (req, res, next) {
@@ -49,7 +72,8 @@ router.post('/register', function (req, res, next) {
   const data = new userModel({
     username: req.body.username,
     email: req.body.email,
-    contact: req.body.contact
+    contact: req.body.contact,
+    name: req.body.fullname
   })
 
   userModel.register(data, req.body.password)
